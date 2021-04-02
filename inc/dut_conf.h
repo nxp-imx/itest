a1b85203be83b64514abdd40657dfe653ef1249d
Loic Boban <loic.boban@nxp.com>
2021-04-02 18:55:44 +0200

    fixup: add missing file

---
+++ inc/dut_conf.h
@@ -0,0 +1,24 @@
+#ifndef DXL_CONF_H
+#define DXL_CONF_H
+#include "itest.h"
+
+// DXL A1 CONF
+extern const char *lava_test_dxl;
+extern const char *lava_get_test_suite_dxl;
+extern const char *lava_dl_flash_bootimg_dxl;
+extern testsuite dxl_ts[];
+
+// QXP C0 CONF
+extern const char *lava_test_qxp_c0;
+extern const char *lava_get_test_suite_qxp_c0;
+extern const char *lava_dl_flash_bootimg_qxp_c0;
+extern testsuite qxp_c0_ts[];
+
+// QXP B0 CONF
+extern const char *lava_test_qxp_b0;
+extern const char *lava_get_test_suite_qxp_b0;
+extern const char *lava_dl_flash_bootimg_qxp_b0;
+extern testsuite qxp_c0_ts[];
+
+
+#endif
