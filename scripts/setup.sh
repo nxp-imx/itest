5a5ba58c959e41a0f4c2e3a4214cd64b5432efbd
Loic Boban <loic.boban@nxp.com>
2021-08-12 16:28:33 +0200

    get lava-tools from rootfs

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 # Get lava-tools
-wget --no-check-certificate -q -O - https://bamboo1.sw.nxp.com/browse/IM-LPLT2/latest/artifact/shared/lava-tools/lava-tools.tar.bz2 | bzip2 -cd | tar xvf -
+cat /opt/lava-tools.tar.bz2 | bzip2 -cd | tar xvf -
 
 # Back up the keystore
 ./lava-tools/lava-persistent-storage/restore_file.sh /etc/itest /etc/v2x_hsm /etc/seco_hsm
