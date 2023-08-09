a7f036abf19a4e2ded91cd1bec1a211a0f4a9ab2
Nikhil Singla <nikhil.singla@nxp.com>
2023-08-09 15:19:06 +0530

    LF-9617: Added RNG test
    Added RNG test of secure_enclave in the itest.
    
    Signed-off-by: Nikhil Singla <nikhil.singla@nxp.com>

---
+++ src/hsm_ele_tests/ele_rng_srv_test.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "itest.h"
+
+#define RANDOM_SIZE 2050
+
+int ele_rng_srv_001(void)
+{
+	hsm_err_t err;
+	uint8_t rng_out_buff[4096] = {0};
+	open_session_args_t args = {0};
+	op_get_random_args_t rng_get_random_args = {0};
+
+	hsm_hdl_t hsm_session_hdl;
+
+	// ELE OPEN SESSION
+	args.session_priority = 0;
+	args.operating_mode = 0;
+	ASSERT_EQUAL(hsm_open_session(&args, &hsm_session_hdl), HSM_NO_ERROR);
+
+	// GET RANDOM Mu SV/SG
+	rng_get_random_args.output = rng_out_buff;
+	rng_get_random_args.random_size = RANDOM_SIZE;
+	ASSERT_EQUAL(hsm_get_random(hsm_session_hdl, &rng_get_random_args),
+		     HSM_NO_ERROR);
+
+	// ELE CLOSE SESSION
+	ASSERT_EQUAL(hsm_close_session(hsm_session_hdl), HSM_NO_ERROR);
+
+	return TRUE_TEST;
+}
+
+
+
+
