bc4a69aecf825da32cae29cd3e93cae58e6ee7d6
Nikhil Singla <nikhil.singla@nxp.com>
2023-08-22 12:07:20 +0530

    LF-9768: Added Brainpool tests
    Added Brainpool tests for signature verification in itest.
    
    Signed-off-by: Nikhil Singla <nikhil.singla@nxp.com>

--- inc/imx8_tests_list.h
+++ inc/imx8_tests_list.h
@@ -11,19 +11,21 @@ int ele_ecb(void);
 int ele_sign_verify(void);
 int ele_cmac(void);
 int ele_hmac(void);
+int ecdsa_brainpool_sign_verify(void);
 
 testsuite imx8_ts[] = {
 /*==============================================================================================*/
 /*============================================== ELE ===========================================*/
 /*==============================================================================================*/
-	{ele_rng_srv_001,	"ele_rng_srv_001",	MX8ULP_A2 | MX93_A1},
-	{ele_rng_perf,		"ele_rng_perf",		MX8ULP_A2 | MX93_A1},
-	{ele_cbc,		"ele_cbc",		MX8ULP_A2 | MX93_A1},
-	{ele_ecb,		"ele_ecb",		MX8ULP_A2 | MX93_A1},
-	{ele_sign_verify,	"ele_sign_verify",	MX8ULP_A2 | MX93_A1},
-	{ele_cmac,		"ele_cmac",		MX8ULP_A2 | MX93_A1},
-	{ele_hmac,		"ele_hmac",		MX8ULP_A2 | MX93_A1},
+{ele_rng_srv_001, "ele_rng_srv_001", MX8ULP_A2 | MX93_A1},
+{ele_rng_perf, "ele_rng_perf", MX8ULP_A2 | MX93_A1},
+{ele_cbc, "ele_cbc", MX8ULP_A2 | MX93_A1},
+{ele_ecb, "ele_ecb", MX8ULP_A2 | MX93_A1},
+{ele_sign_verify, "ele_sign_verify", MX8ULP_A2 | MX93_A1},
+{ele_cmac, "ele_cmac", MX8ULP_A2 | MX93_A1},
+{ele_hmac,	 "ele_hmac", MX8ULP_A2 | MX93_A1},
+{ecdsa_brainpool_sign_verify, "ecdsa_brainpool_sign_verify", MX8ULP_A2 | MX93_A1},
 
-	{NULL, NULL, MX8ULP_A2 | MX93_A1},
+{NULL, NULL, MX8ULP_A2 | MX93_A1},
 };
 #endif
