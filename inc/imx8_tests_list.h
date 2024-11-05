94da36624402b03bed2c35c51e65f531bfe208b2
Nikhil Singla <nikhil.singla@nxp.com>
2024-11-05 12:50:57 +0530

    Add_test: Add performance test for SM3 hash API on V2X
    Added performance test for SM3 hash API on V2X
    
    Signed-off-by: Nikhil Singla <nikhil.singla@nxp.com>

--- inc/imx8_tests_list.h
+++ inc/imx8_tests_list.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2023-2024 NXP
+ * Copyright 2023-2025 NXP
  */
 
 #ifndef IMX8_TESTS_LIST_H
@@ -33,6 +33,7 @@ int v2x_fast_mac_mubuff_v2(void);
 int v2x_ecdsa_verify(void);
 int v2x_hash(void);
 int v2x_ecdsa_verify_brainpool(void);
+int v2x_hash_SM3(void);
 #endif
 
 testsuite imx8_ts[] = {
@@ -63,6 +64,7 @@ testsuite imx8_ts[] = {
 {v2x_ecdsa_verify, "v2x_ecdsa_verify", SOC_IMX95 | SOC_IMX8DXL},
 {v2x_hash, "v2x_hash", SOC_IMX95 | SOC_IMX8DXL},
 {v2x_ecdsa_verify_brainpool, "v2x_ecdsa_verify_brainpool", SOC_IMX95 | SOC_IMX8DXL},
+{v2x_hash_SM3, "v2x_hash_SM3", SOC_IMX95 | SOC_IMX8DXL},
 #endif
 {NULL, NULL, SOC_IMX8ULP | SOC_IMX93 | SOC_IMX95},
 };
