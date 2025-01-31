a604ac2d0e0dc9355eea3392f2c4edda84dee000
Nikhil Singla <nikhil.singla@nxp.com>
2025-01-31 15:03:31 +0530

    LF-14548: Add performance test for V2X HSM AES CBC and AES ECB
    Add performance test for V2X HSM AES CBC and AES ECB.
    
    Signed-off-by: Nikhil Singla <nikhil.singla@nxp.com>

--- inc/imx8_tests_list.h
+++ inc/imx8_tests_list.h
@@ -38,6 +38,8 @@ int v2x_hash_SM3(void);
 int v2x_sm2_sign_verify(void);
 int v2x_sm4_cbc(void);
 int v2x_sm4_ecb(void);
+int v2x_aes_cbc(void);
+int v2x_aes_ecb(void);
 #endif
 
 testsuite imx8_ts[] = {
@@ -73,6 +75,8 @@ testsuite imx8_ts[] = {
 {v2x_sm2_sign_verify, "v2x_sm2_sign_verify", SOC_IMX8DXL | SOC_IMX943},
 {v2x_sm4_cbc, "v2x_sm4_cbc", SOC_IMX8DXL | SOC_IMX943},
 {v2x_sm4_ecb, "v2x_sm4_ecb", SOC_IMX8DXL | SOC_IMX943},
+{v2x_aes_cbc, "v2x_aes_cbc", SOC_IMX8DXL | SOC_IMX943},
+{v2x_aes_ecb, "v2x_aes_ecb", SOC_IMX8DXL | SOC_IMX943},
 #endif
 {NULL, NULL, SOC_IMX8ULP | SOC_IMX93 | SOC_IMX95 | SOC_IMX943},
 };
